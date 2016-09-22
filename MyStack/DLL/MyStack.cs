8a70b0fb79da65dedf7398c3f3d100e26a7ec5de
SheKaij <roninn.junk@gmail.com>
2016-09-22 14:23:57 +0200

    Final

--- MyStack/DLL/MyStack.cs
+++ MyStack/DLL/MyStack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,29 +17,120 @@ namespace MyStack.DLL
             MaxSize = maxSize;
         }
 
-        public int MaxSize { get; set; }
         public int Count { get; private set; }
-        public int Peek { get; private set; }
+
+        private int _maxSize;
+
+        public int MaxSize
+        {
+            get { return _maxSize; }
+            set
+            {
+                if (value < Count)
+                    Count = value;
+                _maxSize = value;
+            }
+        }
+
+        private int _peek;
+        public int Peek
+        {
+            get
+            {
+                if (Count == 0)
+                    throw new InvalidOperationException("The Stack is empty!");
+                return _peek;
+            }
+            private set { _peek = value; }
+        }
 
         public void Push(int item)
         {
+            Stack.Push(item);
             if (Count < MaxSize)
-            {
                 Count++;
-                Peek = item;
-                Stack.Push(item);
-            }
+            Peek = item;
         }
 
         public int Pop()
         {
+            var result = Stack.Pop();
             Count--;
-            return Stack.Pop();
+            if (Count > 0)
+                Peek = Stack.Peek();
+            return result;
         }
         
         public void Clear()
         {
-            throw new NotImplementedException();
+            Count = 0;
+            Stack.Clear();
+        }
+
+        public int AddTop()
+        {
+            int result;
+            switch (Count)
+            {
+                case 0:
+                    result = 0;
+                    break;
+                case 1:
+                    return Peek;
+                default:
+                    result = checked(Pop() + Pop());
+                    break;
+            }
+            Push(result);
+            return result;
+        }
+
+        public int MultiplyTop()
+        {
+            int result;
+            switch (Count)
+            {
+                case 0:
+                    result = 0;
+                    break;
+                case 1:
+                    return Peek;
+                default:
+                    result = checked(Pop() * Pop());
+                    break;
+            }
+            Push(result);
+            return result;
+        }
+
+        public int AddAll()
+        {
+            if (Count == 0)
+                return 0;
+
+            int[] array = Stack.ToArray();
+            int result = 0;
+            
+            for (int i = 0; i < Count; i++)
+            {
+                result = checked(result + array[i]);
+            }
+            return result;
+        }
+
+        public int MultiplyAll()
+        {
+            if (Count == 0)
+                return 0;
+
+            int[] array = Stack.ToArray();
+            int result = array[0];
+
+            for (int i = 1; i < Count; i++)
+            {
+                result = checked(result * array[i]);
+            }
+            return result;
         }
     }
 }
--- MyStackTests/DLL/MyStackTests.cs
+++ MyStackTests/DLL/MyStackTests.cs
@@ -11,91 +11,290 @@ namespace MyStack.DLL.Tests
     [TestClass()]
     public class MyStackTests
     {
-        private MyStack TestStack;
+        private MyStack _testStack;
+        private const int StackSize = 10; // bigger than 2
+        private const int RandomInt = 79; // RandomInt + RandomInt2 < int.MaxValue
+        private const int RandomInt2 = 91;
 
         [TestMethod()]
         public void PushTest()
         {
-            TestStack = new MyStack(10);
-            TestStack.Push(10);
-            Assert.AreEqual(1, TestStack.Count);
-            Assert.AreEqual(10, TestStack.Peek);
+            _testStack = new MyStack(StackSize);
+            _testStack.Push(RandomInt);
+            Assert.AreEqual(RandomInt, _testStack.Peek);
         }
 
         [TestMethod()]
-        public void PushTest2()
+        public void PushTestMore()
         {
-            TestStack = new MyStack(100);
-            int n = 50;
+            _testStack = new MyStack(StackSize);
+            int n = StackSize / 2;
             for (int i = 1; i <= n; i++)
             {
-                TestStack.Push(i);
+                _testStack.Push(i);
             }
-            Assert.AreEqual(50, TestStack.Count);
-            for (int i = 50; i > 0; i--)
+            Assert.AreEqual(n, _testStack.Count);
+            for (int i = n; i > 0; i--)
             {
-                Assert.AreEqual(i, TestStack.Pop());
+                Assert.AreEqual(i, _testStack.Pop());
             }
-            Assert.AreEqual(0, TestStack.Count);
         }
 
         [TestMethod()]
-        public void PushTest3()
+        public void PushTestOverflow()
         {
-            TestStack = new MyStack(100);
-            int n = 200;
+            _testStack = new MyStack(StackSize);
+            int n = StackSize * 2;
             for (int i = 1; i <= n; i++)
             {
-                TestStack.Push(i);
+                _testStack.Push(i);
             }
-            Assert.AreEqual(100, TestStack.Count);
-            for (int i = 100; i > 0; i--)
+            Assert.AreEqual(StackSize, _testStack.Count);
+            for (int i = n; i > n - StackSize; i--)
             {
-                Assert.AreEqual(i, TestStack.Pop());
+                Assert.AreEqual(i, _testStack.Pop());
             }
-            Assert.AreEqual(0, TestStack.Count);
         }
 
         [TestMethod()]
         public void PopTest()
         {
-            TestStack = new MyStack(10);
-            TestStack.Push(10);
-            var item = TestStack.Pop();
-            Assert.AreEqual(0, TestStack.Count);
-            Assert.AreEqual(10, item);
+            _testStack = new MyStack(StackSize);
+            _testStack.Push(RandomInt);
+            var item = _testStack.Pop();
+            Assert.AreEqual(0, _testStack.Count);
+            Assert.AreEqual(RandomInt, item);
         }
 
         [TestMethod()]
         [ExpectedException(typeof(InvalidOperationException))]
-        public void PopTest2()
+        public void PopTestEmpty()
         {
-            TestStack = new MyStack(10);
-            var item = TestStack.Pop();
-            Assert.AreEqual(0, TestStack.Count);
+            _testStack = new MyStack(StackSize);
+            var item = _testStack.Pop();
         }
 
         [TestMethod()]
-        public void PopTest3()
+        public void PopTestMore()
         {
-            TestStack = new MyStack(100);
-            int n = 50;
+            _testStack = new MyStack(StackSize);
+            int n = StackSize / 2;
             for (int i = 1; i <= n; i++)
             {
-                TestStack.Push(i);
+                _testStack.Push(i);
             }
-            Assert.AreEqual(50, TestStack.Count);
-            for (int i = 50; i > 0; i--)
+            for (int i = n; i > 0; i--)
             {
-                Assert.AreEqual(i, TestStack.Pop());
+                Assert.AreEqual(i, _testStack.Pop());
             }
-            Assert.AreEqual(0, TestStack.Count);
+            Assert.AreEqual(0, _testStack.Count);
         }
 
         [TestMethod()]
         public void ClearTest()
         {
-            Assert.Fail();
+            _testStack = new MyStack(StackSize);
+            int n = StackSize / 2;
+            for (int i = 1; i <= n; i++)
+            {
+                _testStack.Push(i);
+            }
+            _testStack.Clear();
+            Assert.AreEqual(0, _testStack.Count);
+        }
+
+        [TestMethod()]
+        public void PeekTest()
+        {
+            _testStack = new MyStack(StackSize);
+            _testStack.Push(10);
+            for (int i = 0; i < StackSize; i++)
+            {
+                Assert.AreEqual(10, _testStack.Peek);
+                Assert.AreEqual(1, _testStack.Count);
+            }
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PeekTestEmpty()
+        {
+            _testStack = new MyStack(StackSize);
+            var item = _testStack.Peek;
+        }
+
+        [TestMethod()]
+        public void PeekTestMore()
+        {
+            _testStack = new MyStack(StackSize);
+            int n = StackSize / 2;
+            for (int i = 1; i <= n; i++)
+            {
+                _testStack.Push(i);
+                Assert.AreEqual(i, _testStack.Peek);
+            }
+            for (int i = n; i > 0; i--)
+            {
+                Assert.AreEqual(i, _testStack.Peek);
+                _testStack.Pop();
+            }
+        }
+
+        [TestMethod()]
+        public void CountTest()
+        {
+            _testStack = new MyStack(StackSize);
+            int n = StackSize * 2;
+            Assert.AreEqual(0, _testStack.Count);
+            for (int i = 1; i <= n; i++)
+            {
+                _testStack.Push(i);
+                if (i <= _testStack.MaxSize)
+                {
+                    Assert.AreEqual(i, _testStack.Count);
+                }
+                else
+                {
+                    Assert.AreEqual(_testStack.MaxSize, _testStack.Count);
+                }
+            }
+            for (int i = _testStack.MaxSize; i > 0; i--)
+            {
+                Assert.AreEqual(i, _testStack.Count);
+                _testStack.Pop();
+            }
+            Assert.AreEqual(0, _testStack.Count);
+        }
+
+        [TestMethod()]
+        public void TestMaxSize()
+        {
+            _testStack = new MyStack(StackSize);
+            int n = StackSize * 2;
+            for (int i = 1; i <= n + 1; i++)
+            {
+                _testStack.Push(i);
+            }
+            Assert.AreEqual(_testStack.Count, _testStack.MaxSize);
+            _testStack.MaxSize = 10;
+            Assert.AreEqual(_testStack.Count, _testStack.MaxSize);
+        }
+
+        [TestMethod()]
+        public void TestAddTop()
+        {
+            _testStack = new MyStack(StackSize);
+            _testStack.Push(RandomInt);
+            _testStack.Push(RandomInt2);
+            var result = _testStack.AddTop();
+            Assert.AreEqual(RandomInt + RandomInt2, result);
+            Assert.AreEqual(1, _testStack.Count);
+            Assert.AreEqual(RandomInt + RandomInt2, _testStack.Peek);
+        }
+
+        [TestMethod()]
+        public void TestAddTopEmplty()
+        {
+            _testStack = new MyStack(StackSize);
+            var result = _testStack.AddTop();
+            Assert.AreEqual(0, result);
+            Assert.AreEqual(1, _testStack.Count);
+            Assert.AreEqual(0, _testStack.Peek);
+        }
+
+        [TestMethod()]
+        public void TestAddTopOne()
+        {
+            _testStack = new MyStack(StackSize);
+            _testStack.Push(RandomInt);
+            var result = _testStack.AddTop();
+            Assert.AreEqual(RandomInt, result);
+            Assert.AreEqual(1, _testStack.Count);
+            Assert.AreEqual(RandomInt, _testStack.Peek);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(OverflowException))]
+        public void TestAddTopOverflow()
+        {
+            _testStack = new MyStack(StackSize);
+            _testStack.Push(int.MaxValue);
+            _testStack.Push(int.MaxValue);
+            var result = _testStack.AddTop();
+        }
+
+        [TestMethod()]
+        public void TestMultiplyTop()
+        {
+            _testStack = new MyStack(StackSize);
+            _testStack.Push(RandomInt);
+            _testStack.Push(RandomInt2);
+            var result = _testStack.MultiplyTop();
+            Assert.AreEqual(RandomInt * RandomInt2, result);
+            Assert.AreEqual(1, _testStack.Count);
+            Assert.AreEqual(RandomInt * RandomInt2, _testStack.Peek);
+        }
+
+        [TestMethod()]
+        public void TestMultiplyTopEmplty()
+        {
+            _testStack = new MyStack(StackSize);
+            var result = _testStack.MultiplyTop();
+            Assert.AreEqual(0, result);
+            Assert.AreEqual(1, _testStack.Count);
+            Assert.AreEqual(0, _testStack.Peek);
+        }
+
+        [TestMethod()] public void TestMultplyTopOne()
+        {
+            _testStack = new MyStack(StackSize);
+            _testStack.Push(RandomInt);
+            var result = _testStack.MultiplyTop();
+            Assert.AreEqual(RandomInt, result);
+            Assert.AreEqual(1, _testStack.Count);
+            Assert.AreEqual(RandomInt, _testStack.Peek);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(OverflowException))]
+        public void TestMultiplyTopOverflow()
+        {
+            _testStack = new MyStack(StackSize);
+            _testStack.Push(int.MaxValue);
+            _testStack.Push(int.MaxValue);
+            var result = _testStack.MultiplyTop();
+        }
+
+        [TestMethod()]
+        public void TestAddAll()
+        {
+            _testStack = new MyStack(StackSize);
+            int sum = 0;
+            int n = 10;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+                _testStack.Push(i);
+            }
+            Assert.AreEqual(n, _testStack.Count);
+            Assert.AreEqual(sum, _testStack.AddAll());
+        }
+
+        [TestMethod()]
+        public void TestMultiplyAll()
+        {
+            _testStack = new MyStack(StackSize);
+            int product;
+            int n = 10;
+            _testStack.Push(1);
+            product = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                product *= i;
+                _testStack.Push(i);
+            }
+            Assert.AreEqual(n, _testStack.Count);
+            Assert.AreEqual(product, _testStack.MultiplyAll());
         }
     }
 }
